9fa04791ce4555086f2e6401365c2ac1362b2772
Ganèche Ramanoudjame <gramanoudjame@gmail.com>
2018-12-22 17:27:56 -0400

    Accesseur et mutateur pour la classe World

--- World.java
+++ World.java
@@ -1,11 +1,24 @@
 public class World{
-    String worldName;
+    private String worldName;
     
-    public World(String worldName){
+    public String getWorldName(){
+        return this.worldName;
+    }
+    
+    public void setWorldName(String worldName){
         this.worldName = worldName;
     }
     
+    private World(){
+        
+    }
+    
+    public World(String worldName){
+        this();
+        this.setWorldName(worldName);
+    }
+    
     public void hello(){
-        System.out.println("Hello world " + this.worldName);
+        System.out.println("Hello world " + this.getWorldName());
     }
 }
